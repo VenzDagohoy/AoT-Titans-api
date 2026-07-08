74250e450b633fb2db6e2b896d49f6b03dff07bb
Venz Andrew Dagohoy <venzdagohoy5@gmail.com>
2026-07-08 18:46:30 +0800

    AoT api

---
+++ index.js
@@ -0,0 +1,39 @@
+const container = document.getElementById("titan-list");
+
+async function getTitans(){
+    const response = await fetch('https://api.attackontitanapi.com/titans');
+    const result = await response.json();
+
+    displayTitans(result.results);
+}
+
+function displayTitans(titansArray) {
+    titansArray.forEach(titan => {
+        const titanCard = document.createElement("div");
+
+        titanCard.classList.add("titan-card");
+
+        let allegianceClass = "allegiance-none";
+        const currentAllegiance = titan.allegiance.toLowerCase();
+        
+        if (currentAllegiance.includes("marley")) {
+            allegianceClass = "allegiance-marley";
+        } else if (currentAllegiance.includes("eldia")) {
+            allegianceClass = "allegiance-eldia";
+        }
+
+        titanCard.innerHTML = `
+            <img src="${titan.img}" alt="${titan.name}" />
+            <div class="card-content">
+                <h2>${titan.name}</h2>
+                <p class="info-row"><strong>Height:</strong> ${titan.height}</p>
+                <p class="info-row"><strong>Abilities:</strong> ${titan.abilities.join(", ")}</p>
+                <span class="allegiance-badge ${allegianceClass}">${titan.allegiance}</span>
+            </div>
+        `;
+
+        container.appendChild(titanCard);
+    });
+}
+
+getTitans();
